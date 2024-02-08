6c1a6c484cd0510545ed9314298de99e0fc331b0
sreejad54 <58872295+sreejad54@users.noreply.github.com>
2024-02-08 19:49:00 +0530

    Create encapMain.java

---
+++ encapsulation/encapMain.java
@@ -0,0 +1,9 @@
+package com.encapsulatiom;
+public class Main {
+
+	public static void main(String[] args) {
+		Student s= new Student();
+		s.setName("sreeja");
+		System.out.println(s.getName());
+	}
+}
