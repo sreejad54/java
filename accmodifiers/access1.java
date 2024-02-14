c03a16b7bc6dd6082597e30afbe84cc2eb4deed3
sreejad54 <58872295+sreejad54@users.noreply.github.com>
2024-02-14 11:08:26 +0530

    Add files via upload

---
+++ accmodifiers/access1.java
@@ -0,0 +1,17 @@
+package accmodifiers;
+
+public class access1 {
+	public int hours =5;
+	public int minutes=79;
+	
+	public int gethours()
+	{
+		return hours;
+		
+	}
+	public int getminutes()
+	{
+		return minutes;
+	}
+
+}
---
+++ accmodifiers/access2.java
@@ -0,0 +1,13 @@
+package accmodifiers;
+
+public class access2 {
+	
+	public static void main(String[] args) {
+		
+		access1 a1 = new access1();
+		System.out.println(a1.gethours());
+		System.out.println(a1.getminutes());
+		
+	}
+
+}
