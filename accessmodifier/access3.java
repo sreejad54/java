026494386891eaa14af6cf285ece713c55bfc50f
sreejad54 <58872295+sreejad54@users.noreply.github.com>
2024-02-14 11:02:57 +0530

    Add files via upload

---
+++ accessmodifier/access3.java
@@ -0,0 +1,14 @@
+package com.accessmodifier;
+
+import accmodifiers.access1;
+
+public class access3 extends access1 {
+	
+	public static void main(String[] args) {
+		
+		access3 a1 = new access3();
+		System.out.println( a1.gethours());
+		System.out.println(a1.getminutes());
+	}
+
+}
